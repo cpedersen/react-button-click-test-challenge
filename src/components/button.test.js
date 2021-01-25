8dab0dbddaf14838c92f6f2323c9311f33b0009b
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-01-25 11:43:09 -0500

    Implemented challenge

--- src/components/button.test.js
+++ src/components/button.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-
 import Button from './button';
 
 describe('<Button />', () => {
     it('Should call props.onClick when the button is clicked', () => {
-        const wrapper = shallow(<Button />);
+        const onClick = jest.fn();
+        const wrapper = shallow(<Button onClick={onClick}/>);
         wrapper.find('button').simulate('click');
+        expect(onClick).toHaveBeenCalledTimes(1);
     });
 });
