1083a3e2db790ff87e6f4fd18f42e05d3c1508c0
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-03-11 17:24:08 -0500

    Add react challenge

--- src/components/button.test.js
+++ src/components/button.test.js
@@ -1,12 +1,27 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Button from "./button";
+
+describe("<Button />", () => {
+  it("Should call props.onClick when the button is clicked", () => {
+    const onClick = jest.fn();
+    const wrapper = shallow(<Button onClick={onClick} />);
+    wrapper.find("button").simulate("click");
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+});
+
+/* PREVIOUS VERSION that didn't work:
+
 import React from 'react';
 import {shallow} from 'enzyme';
+
 import Button from './button';
 
 describe('<Button />', () => {
     it('Should call props.onClick when the button is clicked', () => {
-        const onClick = jest.fn();
-        const wrapper = shallow(<Button onClick={onClick}/>);
+        const wrapper = shallow(<Button />);
         wrapper.find('button').simulate('click');
-        expect(onClick).toHaveBeenCalledTimes(1);
     });
 });
+*/
